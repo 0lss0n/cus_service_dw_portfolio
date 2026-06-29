379a3b1903cf85da4e407b815618913dd9a8e5ee
0lss0n <124281177+0lss0n@users.noreply.github.com>
2026-06-29 14:31:04 +0200

    ddl_brons.sql

--- skript/brons/ddl_brons.sql
+++ skript/brons/ddl_brons.sql
@@ -7,26 +7,27 @@ Kör det här skriptet för att omdefiniera DDL-strukturen för 'brons'-tabeller
 
 */
 
-IF OBJECT_ID ('brons.crm_cust_info' , 'U') IS NOT NULL
-	DROP TABLE brons.crm_cust_info;
+IF OBJECT_ID ('brons.crm_con_per' , 'U') IS NOT NULL
+	DROP TABLE brons.crm_con_per;
 GO
 
-CREATE TABLE brons.crm_cust_info(
+CREATE TABLE brons.crm_con_per(
+	cont_id INT,
 	cust_id INT,
 	comp_nm NVARCHAR(50),
-	country NVARCHAR(50),
-	segment NVARCHAR(50),
-	sector NVARCHAR(50),
-	status NVARCHAR(50),
-	cre_dt DATE
+	first_name NVARCHAR(50),
+	last_name NVARCHAR(50),
+	role NVARCHAR(50),
+	email NVARCHAR(50),
+	phone NVARCHAR(50)
 );
 GO
 
 IF OBJECT_ID ('brons.crm_channel' , 'U') IS NOT NULL
 	DROP TABLE brons.crm_channel;
 CREATE TABLE brons.crm_channel(
-	cha_id NVARCHAR(50),
-	cha_nm NVARCHAR(50)
+	chan_id NVARCHAR(50),
+	chan_nm NVARCHAR(50)
 );
 GO
 
@@ -36,15 +37,13 @@ CREATE TABLE brons.crm_cus_ser_iss(
 	case_id NVARCHAR(50),
 	ship_id NVARCHAR(50),
 	cust_id INT,
-	item_id NVARCHAR(50),
+	prod_id NVARCHAR(50),
 	chan_id NVARCHAR(50),
 	open_date DATETIME2,
 	closed_date DATETIME2,
 	status NVARCHAR(50),
-	deli_prob INT,
-	invoice_q INT,
-	c_m INT,
-	complaints INT
+	csat INT,
+	case_type NVARCHAR(50)
 );
 GO
 
@@ -56,8 +55,37 @@ CREATE TABLE brons.erp_cp_dest(
 	origin NVARCHAR(50),
 	destination NVARCHAR(50),
 	country NVARCHAR(50),
-	item_id NVARCHAR(50),
-	deli_sta NVARCHAR(50),
-	delivery_date DATE
+	prod_id NVARCHAR(50),
+	d_status NVARCHAR(50),
+	d_date DATE
+);
+GO
+
+IF OBJECT_ID ('brons.erp_cust_info' , 'U') IS NOT NULL
+	DROP TABLE brons.erp_cust_info;
+CREATE TABLE brons.erp_cust_info(
+	cust_id INT,
+	comp_nm NVARCHAR(50),
+	country NVARCHAR(50),
+	segment NVARCHAR(50),
+	sector NVARCHAR(50),
+	status NVARCHAR(50),
+	cre_dt DATE
+);
+GO
+
+IF OBJECT_ID ('brons.erp_ship_id' , 'U') IS NOT NULL
+	DROP TABLE brons.erp_ship_id;
+CREATE TABLE brons.erp_ship_id(
+	ship_id NVARCHAR (50),
+	cust_id INT,
+	prod_id NVARCHAR(50),
+	origin NVARCHAR(50),
+	destination NVARCHAR(50),
+	country NVARCHAR(50),
+	d_status NVARCHAR(50),
+	d_date DATE,
+	weight INT,
+	volume INT
 );
 GO
