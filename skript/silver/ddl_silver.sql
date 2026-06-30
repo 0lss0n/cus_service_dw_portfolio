c840489e810b6ac2cb73a77e3170567c1f90134b
0lss0n <124281177+0lss0n@users.noreply.github.com>
2026-06-30 13:19:49 +0200

    ddl_silver.sql

--- skript/silver/ddl_silver.sql
+++ skript/silver/ddl_silver.sql
@@ -12,14 +12,15 @@ IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 GO
 
-CREATE TABLE silver.crm_cust_info(
+CREATE TABLE silver.crm_con_per(
+	cont_id INT,
 	cust_id INT,
 	comp_nm NVARCHAR(50),
-	country NVARCHAR(50),
-	segment NVARCHAR(50),
-	sector NVARCHAR(50),
-	status NVARCHAR(50),
-	cre_dt DATE,
+	first_name NVARCHAR(50),
+	last_name NVARCHAR(50),
+	role NVARCHAR(50),
+	email NVARCHAR(50),
+	phone NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -27,8 +28,8 @@ GO
 IF OBJECT_ID ('silver.crm_channel' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_channel;
 CREATE TABLE silver.crm_channel(
-	cha_id NVARCHAR(50),
-	cha_nm NVARCHAR(50),
+	chan_id NVARCHAR(50),
+	chan_nm NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -39,15 +40,13 @@ CREATE TABLE silver.crm_cus_ser_iss(
 	case_id NVARCHAR(50),
 	ship_id NVARCHAR(50),
 	cust_id INT,
-	item_id NVARCHAR(50),
+	prod_id NVARCHAR(50),
 	chan_id NVARCHAR(50),
 	open_date DATETIME2,
 	closed_date DATETIME2,
 	status NVARCHAR(50),
-	deli_prob INT,
-	invoice_q INT,
-	c_m INT,
-	complaints INT,
+	csat NVARCHAR(50),
+	case_type NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -60,9 +59,41 @@ CREATE TABLE silver.erp_cp_dest(
 	origin NVARCHAR(50),
 	destination NVARCHAR(50),
 	country NVARCHAR(50),
-	item_id NVARCHAR(50),
-	deli_sta NVARCHAR(50),
-	delivery_date DATE,
+	prod_id NVARCHAR(50),
+	d_status NVARCHAR(50),
+	d_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID ('silver.erp_cust_info' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_info;
+CREATE TABLE silver.erp_cust_info(
+	cust_id INT,
+	comp_nm NVARCHAR(50),
+	country NVARCHAR(50),
+	segment NVARCHAR(50),
+	sector NVARCHAR(50),
+	status NVARCHAR(50),
+	cre_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID ('silver.erp_ship_id' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_ship_id;
+CREATE TABLE silver.erp_ship_id(
+	ship_id NVARCHAR(50),
+	case_id NVARCHAR(50),
+	cust_id INT,
+	prod_id NVARCHAR(50),
+	origin NVARCHAR(50),
+	destination NVARCHAR(50),
+	country NVARCHAR(50),
+	d_status NVARCHAR(50),
+	d_date DATE,
+	weight DECIMAL(6,3),
+	volume DECIMAL(6,3),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
